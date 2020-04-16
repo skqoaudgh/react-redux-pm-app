4cd590d8d862b2d41b4ee9eed0b0eaf920f75145
myungho <skqoaudgh2@hanmail.net>
2020-04-16 16:17:27 +0900

    Redux를 통해 프로젝트 목록을 컴포넌트 간 공유할 수 있도록

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Notification from './Notification';
 import ProjectList from '../projects/ProjectList';
 
 class Dashboard extends Component {
   render() {
+    const { projects } = this.props;
     return (
       <div className="dashboard container">
         <div className="row">
           <div className="col s12 m6">
-            <ProjectList />
+            <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
             <Notification />
@@ -21,4 +23,10 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+  return {
+    projects: state.project.projects,
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from './store/reducers/rootReducer';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
