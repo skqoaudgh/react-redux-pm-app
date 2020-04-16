d89f41806d4b10f1c6c1d2f2dc902f46dc8ac291
myungho <skqoaudgh2@hanmail.net>
2020-04-16 17:43:48 +0900

    상세 프로젝트 컴포넌트에 데이터베이스 값이 들어가도록

--- src/index.js
+++ src/index.js
@@ -33,12 +33,10 @@ const rrfProps = {
 };
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-        <App />
-      </ReactReduxFirebaseProvider>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
