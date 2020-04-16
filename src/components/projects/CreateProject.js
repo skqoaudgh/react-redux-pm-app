a42e2a54ab91a9619d0b2e8e9a8932710dd08eb5
myungho <skqoaudgh2@hanmail.net>
2020-04-16 15:23:30 +0900

    프로젝트 생성 컴포넌트 추가

---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+export class CreateProject extends Component {
+  state = {
+    title: '',
+    content: '',
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">새 프로젝트 생성</h5>
+          <div className="input-field">
+            <label htmlFor="title">제목</label>
+            <input type="text" id="title" onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="content">프로젝트 내용</label>
+            <textarea
+              id="content"
+              className="materialize-textarea"
+              onChange={this.handleChange}
+            ></textarea>
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Create</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default CreateProject;
