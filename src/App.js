bfb7bdb8bc7b3413708f85ba440d6291061276b5
myungho <skqoaudgh2@hanmail.net>
2020-04-16 14:14:08 +0900

    프로젝트 초기 설정 및 기본 구조 추가

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BroswerRouter } from 'react-router-dom';
+
+function App() {
+  return (
+    <BroswerRouter>
+      <div>
+        <h1>CadaPlan</h1>
+      </div>
+    </BroswerRouter>
+  );
+}
+
+export default App;
