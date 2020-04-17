788694514825090982c11e242841fa323336fc10
myungho <skqoaudgh2@hanmail.net>
2020-04-17 15:16:26 +0900

    파이어베이스에서 데이터를 받기전에 화면이 렌더링되는 오류 수정

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import App from './App';
 import './index.css';
 
 import { createStore, applyMiddleware, compose } from 'redux';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import thunk from 'redux-thunk';
 import {
   createFirestoreInstance,
   getFirestore,
   reduxFirestore,
 } from 'redux-firestore';
-import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import {
+  ReactReduxFirebaseProvider,
+  getFirebase,
+  isLoaded,
+} from 'react-redux-firebase';
 import fbConfig from './config/fbConfig';
 import firebase from 'firebase/app';
 
@@ -30,12 +34,23 @@ const rrfProps = {
   config: fbConfig,
   dispatch: store.dispatch,
   createFirestoreInstance,
+  userProfile: 'users',
+  presence: 'presence',
+  sessions: 'sessions',
 };
+
+function AuthIsLoaded({ children }) {
+  const auth = useSelector((state) => state.firebase.auth);
+  if (!isLoaded(auth)) return <div>Loading Screen...</div>;
+  return children;
+}
 
 ReactDOM.render(
   <Provider store={store}>
     <ReactReduxFirebaseProvider {...rrfProps}>
-      <App />
+      <AuthIsLoaded>
+        <App />
+      </AuthIsLoaded>
     </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById('root')
