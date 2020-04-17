5f350e77dbea9487fdcf1b0fce66afc0e038d490
myungho <skqoaudgh2@hanmail.net>
2020-04-17 15:11:43 +0900

    로그인 상태에 따라 네비게이션 바에 다른 버튼이 보이도록

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,23 +5,25 @@ import { connect } from 'react-redux';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { auth } = props;
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />;
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to="/" className="brand-logo">
           CadaPlan
         </Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   );
 };
 
 const mapStateToProps = (state) => {
-  console.log(state);
-  return {};
+  return {
+    auth: state.firebase.auth,
+  };
 };
 
 export default connect(mapStateToProps)(Navbar);
