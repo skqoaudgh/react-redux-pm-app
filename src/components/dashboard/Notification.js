43adad10e61f75ecb7274ef4fb9a81a022e702d0
myungho <skqoaudgh2@hanmail.net>
2020-04-17 16:35:42 +0900

    알람 컴포넌트 기본 구조 작성

--- src/components/dashboard/Notification.js
+++ src/components/dashboard/Notification.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Notification = () => {
   return (
-    <div>
-      <p>Notifications</p>
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">알람</span>
+          <ul>
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
