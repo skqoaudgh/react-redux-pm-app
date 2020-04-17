aa2345042ce19f86eab803e56ac4304b0b1d5a45
myungho <skqoaudgh2@hanmail.net>
2020-04-17 16:04:44 +0900

    프로젝트 생성 기능  추가. 파이어베이스에서 데이터를 받아 표기하도록

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -19,6 +19,7 @@ export class CreateProject extends Component {
   handleSubmit = (e) => {
     e.preventDefault();
     this.props.createProject(this.state);
+    this.props.history.push('/');
   };
 
   render() {
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,13 +2,15 @@ export const createProject = (project) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // 데이터베이스 비동기 호출 로직 구현
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     firestore
       .collection('projects')
       .add({
         ...project,
-        authorFirstName: 'Myungho',
-        authorLastName: 'Bae',
-        authorId: 12345,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId: authorId,
         createdAt: new Date(),
       })
       .then(() => {
