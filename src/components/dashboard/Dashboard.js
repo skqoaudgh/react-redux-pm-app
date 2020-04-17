871e3bf43cb3a81760037bc4ab4344c16be16d90
myungho <skqoaudgh2@hanmail.net>
2020-04-17 17:12:12 +0900

    파이어베이스 functions를 이용하여 알람 기능 추가

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -10,7 +10,7 @@ import ProjectList from '../projects/ProjectList';
 
 class Dashboard extends Component {
   render() {
-    const { projects, auth } = this.props;
+    const { projects, auth, notifications } = this.props;
     if (!auth.uid) return <Redirect to="/signin" />;
     return (
       <div className="dashboard container">
@@ -19,7 +19,7 @@ class Dashboard extends Component {
             <ProjectList projects={projects} />
           </div>
           <div className="col s12 m5 offset-m1">
-            <Notification />
+            <Notification notifications={notifications} />
           </div>
         </div>
       </div>
@@ -31,10 +31,17 @@ const mapStateToProps = (state) => {
   return {
     projects: state.firestore.ordered.projects,
     auth: state.firebase.auth,
+    notifications: state.firestore.ordered.notifications,
   };
 };
 
 export default compose(
   connect(mapStateToProps),
-  firestoreConnect([{ collection: 'projects' }])
+  firestoreConnect([
+    { collection: 'projects' },
+    {
+      collection: 'notifications',
+      limit: 3,
+    },
+  ])
 )(Dashboard);
--- src/components/dashboard/Notification.js
+++ src/components/dashboard/Notification.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import moment from 'moment';
 
-const Notification = () => {
+const Notification = (props) => {
+  const { notifications } = props;
   return (
     <div className="section">
       <div className="card z-depth-0">
         <div className="card-content">
           <span className="card-title">알람</span>
           <ul>
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
-            <li>Notification</li>
+            {notifications &&
+              notifications.map((item) => (
+                <li key={item.id}>
+                  <span className="pink-text">{item.user} </span>
+                  <span>{item.content}</span>
+                  <div className="grey-text note-date">
+                    {moment(item.time.toDate()).fromNow()}
+                  </div>
+                </li>
+              ))}
           </ul>
         </div>
       </div>
